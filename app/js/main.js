dbb11c8138fb4da5f76d5f7faa31657b6681dc6d
ucozlab <artem@ucoz-team.net>
2016-04-24 11:14:40 +0300

    not working

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,22 @@
 function hasClass(elem, klass) {
     return (" " + elem.className + " ").indexOf(" " + klass + " ") > -1;
 }
-function addRemoveClass(who,andclass){
+
+function addRemoveClass(who, andclass) {
     if (hasClass(who, andclass)) {
         who.classList.remove(andclass);
     } else {
         who.classList.add(andclass);
     }
 }
+
 function mainMenu(elem) {
-    addRemoveClass(elem,'opened');
+    addRemoveClass(elem, 'opened');
     return false;
 }
 
-function changeView(elem) {
-    var stateParameters = {
-        foo: "bar"
-    };
-    var uri = elem; // предварительно формирование uri
-    history.pushState(stateParameters, "New page title", uri);
-    history.pathname = uri;
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 /*indexedDB https://habrahabr.ru/post/213515/ */
@@ -28,30 +25,31 @@ var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || win
 if (!indexedDB) {
     alert("Your browser doesn't support a stable version of IndexedDB");
 }
-function connectDB(f){
-	var request = indexedDB.open("myBase", 1);
-	request.onerror = function(err){
-		console.log(err);
-	};
-	request.onsuccess = function(){
-		// При успешном открытии вызвали коллбэк передав ему объект БД
-		f(request.result);
-	}
-	request.onupgradeneeded = function(e){
-		// Если БД еще не существует, то создаем хранилище объектов.
-		e.currentTarget.result.createObjectStore("myObjectStore", { keyPath: "key" });
-		connectDB(f);
-	}
+
+function connectDB(f) {
+    var request = indexedDB.open("myBase", 1);
+    request.onerror = function (err) {
+        console.log(err);
+    };
+    request.onsuccess = function () {
+        // При успешном открытии вызвали коллбэк передав ему объект БД
+        f(request.result);
+    }
+    request.onupgradeneeded = function (e) {
+        // Если БД еще не существует, то создаем хранилище объектов.
+        e.currentTarget.result.createObjectStore("myObjectStore", {
+            keyPath: "key"
+        });
+        connectDB(f);
+    }
 }
 /*end*/
 
-function app(){
+/*app*/
+function app() {
     var section = document.getElementById, nav;
-
-    window.addEventListener('hashchange', hashChangeCallback, false);
-    location.hash = '!/';
 }
-app.changeView = function(elem) {
+app.changeView = function (elem) {
     var targ = elem.getAttribute("href");
     var sect = document.getElementById(targ);
     var siblings = sect.parentNode.childNodes;
@@ -63,7 +61,33 @@ app.changeView = function(elem) {
         siblings[i].classList.remove('active');
     }
     sect.classList.add('active');
+    location.hash = '!/'+targ+'';
+    var stateParameters = {
+        foo: "bar"
+    };
+
+    var uri = elem; // предварительно формирование uri
+    history.pushState(stateParameters, ""+capitalizeFirstLetter(targ)+" - MyGallery SPA", uri);
+    history.pathname = uri;
+
 }
+app.init = function () {
+
+    function hashChangeCallback() {
+        if (/^\#\!/.test(location.hash)) {
+            var route = location.hash.substr(2),
+                anchor = $('a[name="!' + route + '"]');
+            if (anchor.length) {
+                $('li.active', $nav).removeClass('active');
+                $('li:has(a[href="#!' + route + '"])', $nav).addClass('active');
+                $sections.hide();
+                anchor.closest('section').show();
+            }
+        }
+    }
+    window.addEventListener('hashchange', hashChangeCallback, false);
+}
+app.init(); //start app
 
 /*not to work but to read  !! https://habrahabr.ru/post/200720/
 
