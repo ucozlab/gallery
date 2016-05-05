2a0e77e4db29f9499addce24aecd679c8df41b99
Artem <la-3111@mail.ru>
2016-05-05 17:49:36 +0300

    trying load from files

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,26 @@ function app() {
 
 }
 
+app.loadPage = function (href) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "" + href + ".html", false);
+    xmlhttp.send();
+    return xmlhttp.onreadystatechange = function (e) {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            return xmlhttp.responseText;
+        } else {
+            alert('Can not load page, smthng is wrong');
+        }
+    }
+
+}
+
 app.changeView = function (text, nothing) { //changeView
-    var who = document.getElementById(text);
+    var content = document.getElementById('content');
+    content.innerHTML = app.loadPage(text);
+    //var who = document.getElementById(text);
     var link = document.querySelectorAll('#nav ul li a[href="' + text + '"]')[0] || nothing;
-    siblingsAddRemoveClass(who, 'active');
+    //siblingsAddRemoveClass(who, 'active');
     if (typeof nothing != 'undefined') {
         siblingsAddRemoveClass(link.parentElement, 'active');
     }
@@ -45,7 +61,7 @@ app.init = function () {
     }
     //check if hass local stored item then append them to page
     if (localStorage.length) {
-        for ( var i = 0; i < localStorage.length; i++ ) {
+        for (var i = 0; i < localStorage.length; i++) {
             var parsed = JSON.parse(localStorage.getItem(localStorage.key(i)));
             app.addPhoto(parsed);
             GLOBALID++;
@@ -57,26 +73,26 @@ app.addPhoto = function (params) {
     var div = document.createElement('div');
     div.className = "col-md-3";
     div.innerHTML = '<div class="block" id="item' + GLOBALID + '">' + '<div class="b-img"><a href="javacript:void(0)" onclick="app.photoPage(\'item' + GLOBALID + '\')"><img id="myImage" src="' + params.img + '"></a></div>' + '<div class="b-name h2">' + params.name + '</div>' + '<div class="b-cat additional"><i class="material-icons inline-block">folder</i> ' + params.cat + '</div>' + '<div class="b-desc">' + params.desc + '</div>' + '</div>';
-    append(div,'albums');
+    append(div, 'albums');
 }
 app.addToStorage = function (params) {
-    localStorage.setItem('item'+GLOBALID+'',JSON.stringify(params));
+    localStorage.setItem('item' + GLOBALID + '', JSON.stringify(params));
     GLOBALID++;
     var loaderdiv = document.getElementById('addform');
     addRemoveClass(loaderdiv, 'loader');
 }
-app.photoPage = function(item) {
+app.photoPage = function (item) {
     app.changeView('photo');
     //go to localstorage and output data from it
     var parsed = JSON.parse(localStorage.getItem(item));
     var div1 = document.createElement('div');
     div1.className = "b-img";
     div1.innerHTML = '<img src="' + parsed.img + '">';
-    append(div1,'mainphoto');
+    append(div1, 'mainphoto');
     var div2 = document.createElement('div');
     div2.className = "b-content";
     div2.innerHTML = '<div class="b-name h2">' + parsed.name + '</div><div class="b-cat additional"><i class="material-icons inline-block">folder</i> ' + parsed.cat + '</div><div class="b-desc">' + parsed.desc + '</div>';
-    append(div2,'maindesc');
+    append(div2, 'maindesc');
 }
 
 app.init(); //initialize app
